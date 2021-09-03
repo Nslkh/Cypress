d8f5898ddd8976418e2e362fd5e58178b4a27686
Nslkh <olluzran@gmail.com>
2021-09-03 21:51:54 +0500

    my fisrt auto test

---
+++ cypress/integration/my_first_test.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" /> 
+
+
+it('apelsin.uz test' , function()  {
+    cy.visit('https://google.com')
+    cy.get('.gLFyf').type('Automation step by step{enter}') 
+    
+})
